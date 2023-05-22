feef807e4ff50982e17bbbcc6068fb9fd95e16a0
CraciDenisStefan <crdenis13@gmail.com>
2023-05-22 14:23:32 +0200

    Esercizio mail completo

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+let email = [`stanlee@email.com`, `denis@email.com`, `peterparker@email.com`, `tonystark@email.com`, `nickfury@email.com`, `steverogers@email.com`, `scottlang@email.com`, `brucebanner@email.com`, `thor@email.com`]
+let avengers = prompt(`Ciao Avenger, inserisci la tua email :`)
+let flag = false;
+for( let i = 0; i < email.length; i++ ){
+    if(email[i] === avengers){
+        flag = true;
+
+    } 
+}        
+if(flag === true){
+        console.log(`Benvenuto Avenger !`)
+    }else{
+        console.log(`Non sei un Avenger`)
+    }
