e2f5ccf02543d66d75693b7acf805f68cb07e1ec
CraciDenisStefan <crdenis13@gmail.com>
2023-05-22 14:54:19 +0200

    Esercizio dadi completo

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let utente = Math.floor(Math.random() * 6) + 1;
+console.log(`Utente: ${utente}`)
+
+let computer = Math.floor(Math.random() * 6) + 1;
+console.log(`Computer: ${computer}`)
+
+if(utente > computer){
+    console.log(`L'utente ha vinto !`)
+}else if(utente < computer){
+    console.log(`Il computer ha vinto !`)
+}else if(utente = computer){
+    console.log(`Pareggio`)
+}
